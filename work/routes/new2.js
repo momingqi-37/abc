dd595fea1d4715dec7cf37e192fd41ed62d8f81b
momingqi-37 <1271494881@qq.com>
2021-05-24 19:41:04 +0800

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=50f4527565fe04ea3230&state=d724cce6-e3a9-4c81-8300-0ef44510f605

--- work/routes/new2.js
+++
@@ -1,9 +0,0 @@
-const { render } = require('ejs');
-var express = require('express');
-var router =express.Router();
-
-router.get('/',(req,res,next)=>{
-    res.render('2.html');
-})
-
-module.exports = router;
--- work/routes/new3.js
+++
@@ -1,9 +0,0 @@
-const { render } = require('ejs');
-var express = require('express');
-var router =express.Router();
-
-router.get('/',(req,res,next)=>{
-    res.render('3.html');
-})
-
-module.exports = router;
